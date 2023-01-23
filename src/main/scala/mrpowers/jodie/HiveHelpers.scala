a2cd22b3f9f303cf47715f19da67203c52175113
Brayan Jules <brayanjuls@users.noreply.github.com>
2023-01-23 12:35:53 -0300

    Registering a delta table to hive metastore. (#21)
    * feature: registering a delta table to hive metastore.
    
    Signed-off-by: brayan_jules <brayan_jules@epam.com>
    
    * feature: added support for parquet table to be registered into hive.
    
    Signed-off-by: brayan_jules <brayan_jules@epam.com>

--- src/main/scala/mrpowers/jodie/HiveHelpers.scala
+++ src/main/scala/mrpowers/jodie/HiveHelpers.scala
@@ -1,5 +1,7 @@
 package mrpowers.jodie
 
+import org.apache.spark.sql.catalyst.analysis.TableAlreadyExistsException
+import org.apache.spark.sql.delta.DeltaAnalysisException
 import org.apache.spark.sql.{AnalysisException, SparkSession}
 
 object HiveHelpers {
@@ -46,7 +48,31 @@ object HiveHelpers {
     }
   }
 
+  def registerTable(tableName:String, tablePath:String, provider:HiveProvider = HiveProvider.DELTA):Unit = {
+    if(tablePath.isEmpty || tableName.isEmpty){
+      throw JodieValidationError("tableName and tablePath input parameters must not be empty")
+    }
+    try{
+      if(provider == HiveProvider.DELTA){
+        SparkSession.active.sql(s"CREATE TABLE $tableName using delta location '$tablePath'")
+      }else{
+        SparkSession.active.catalog.createTable(tableName,tablePath)
+      }
+
+    }catch {
+      case e:DeltaAnalysisException => throw JodieValidationError(s"table:$tableName location:$tablePath is not a delta table")
+      case e:TableAlreadyExistsException => throw JodieValidationError(s"table:$tableName already exits")
+    }
+  }
+
   sealed abstract class HiveTableType(val label: String)
+
+  sealed abstract class HiveProvider(val label: String)
+
+  object HiveProvider{
+    final case object DELTA extends HiveProvider(label = "delta")
+    final case object PARQUET extends HiveProvider(label = "parquet")
+  }
 
   object HiveTableType {
     final case object MANAGED extends HiveTableType(label = "MANAGED")
